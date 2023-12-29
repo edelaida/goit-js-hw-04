534468163136f0c8ff127fcd8747758704b0ddbc
Levina Tani <edelaida@gmail.com>
2023-12-29 19:57:23 +0200

    Object

--- js/task-1.js
+++ js/task-1.js
@@ -2,13 +2,11 @@
 //залишилося написати програмне забезпечення для відділу продажів.
 // Оголоси функцію makeTransaction, яка очікує два параметри,
 //значення яких будуть задаватися під час її виклику:
-
 // quantity— перший параметр, число, що містить кількість замовлених дроїдів
 // pricePerDroid — другий параметр, число, що містить вартість одного дроїда
 // Доповни код функції так, щоб вона повертала рядок з повідомленням
 //про покупку ремонтних дроїдів:
 //"You ordered <quantity> droids worth <totalPrice> credits!", де:
-
 // <totalPrice> — це загальна вартість замовлення,
 // тобто вартість усіх замовлених дроїдів
 
@@ -27,12 +25,10 @@
 // функцію makeTransaction(quantity, pricePerDroid, customerCredits),
 //     яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
 // Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
-
 // quantity — кількість замовлених дроїдів
 // pricePerDroid — ціна одного дроїда
 // customerCredits — сума коштів на рахунку клієнта
 // Доповни функцію таким чином:
-
 // Оголоси змінну для зберігання загальної суми замовлення
 // (загальна вартість усіх замовлених дроїдів) і задай їй вираз розрахунку цієї суми.
 // Додай перевірку, чи зможе клієнт оплатити замовлення:
@@ -60,19 +56,60 @@
 
 // Напиши функцію slugify(title), яка приймає заголовок статті, параметр title
 //  і повертає slug, створений із цього рядка.
-
 // Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
 // Усі символи slug повинні бути в нижньому регістрі.
 // Усі слова slug повинні бути розділені тире.
  
-function slugify(title) {
-    const message = title.split(" ");
-    const slugMess = message.join("-");
-    return (slugMess.toLowerCase());
- }
+// function slugify(title) {
+//     const message = title.split(" ");
+//     const slugMess = message.join("-");
+//     return (slugMess.toLowerCase());
+//  }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
 
+//********************* Задача 1. Пакування товарів */
+
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює,
+//     чи помістяться всі товари в контейнер при пакуванні.
+
+// Функція оголошує два параметри:
+
+// products — обєкт, у якому ключі містять назви товарів, а їхні значення —
+// кількість цих товарів.Наприклад, { apples: 2, grapes: 4 }.
+//     containerSize — число, максимальна кількість одиниць товарів, яку в себе
+// може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в
+// контейнер.Тобто порахувати загальну кількість товарів в обєкті products і
+// повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
+function isEnoughCapacity(products, containerSize) {
+  let sum = 0;
+  for (const key in products) {
+    sum += products[key];
+  }
+   if (sum <= containerSize) {
+     return "true";
+   } else {
+     return "false";
+   }
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
--- js/task-2.js
+++ js/task-2.js
@@ -57,26 +57,69 @@
 //  копію масиву з довжиною maxLength елементів.
 // В іншому випадку функція повинна повернути весь новий масив.
 
-function makeArray(firstArray, secondArray, maxLength) {
-    const newArray = [];
-    let num = firstArray.length;
-    for (let i = 0; i < maxLength; i++) {
-       if (i < firstArray.length) {
-           newArray.push(firstArray[i]);
-       } else {
-           let j = i - num;
-           newArray.push(secondArray[j]);
-       }     
-   }
-    return newArray;
+// function makeArray(firstArray, secondArray, maxLength) {
+//     const newArray = [];
+//     let num = firstArray.length;
+//     for (let i = 0; i < maxLength; i++) {
+//        if (i < firstArray.length) {
+//            newArray.push(firstArray[i]);
+//        } else {
+//            let j = i - num;
+//            newArray.push(secondArray[j]);
+//        }
+//    }
+//     return newArray;
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+// ****************** Задача 2. Розрахунок калорій **********
+
+// Напиши функцію calcAverageCalories(days), яка повертає середньодобове
+// значення кількості калорій, які спортсмен споживав протягом тижня.
+// Функція очікує один параметр: days — масив обєктів.Кожен обєкт описує
+// день тижня та кількість калорій calories, спожитих спортсменом, у цей день.
+ 
+function calcAverageCalories(days) {
+  let ration = 0;
+  let number = 0;
+  for (const key of days) {
+    ration += key.calories; 
+    number += 1;
+  }
+  return (ration / number);
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
 
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
 
- 
+console.log(
+  calcAverageCalories([])
+); // 0
+
